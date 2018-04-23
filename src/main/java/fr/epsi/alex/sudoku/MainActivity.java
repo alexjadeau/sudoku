7d0675542f5b1c81cbc3e31191dc3e77bafdec68
Your Name <you@example.com>
2018-04-23 08:50:24 +0200

    App

---
+++ src/main/java/fr/epsi/alex/sudoku/MainActivity.java
@@ -0,0 +1,15 @@
+package fr.epsi.alex.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button easy = (Button)findViewById(R.id.easy);
+    }
+}
